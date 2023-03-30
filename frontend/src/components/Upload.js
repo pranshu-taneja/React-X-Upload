5b033b5fa70fcebc86c2526342ea9c1316492f47
pranshu-taneja <pranshu45taneja@gmail.com>
2023-03-30 23:40:49 +0530

    Project Update

--- frontend/src/components/Upload.js
+++ frontend/src/components/Upload.js
@@ -1,30 +1,47 @@
-import React, { useState } from 'react'
-import './Upload.css'
+import React, { useState } from "react";
+import "./Upload.css";
 
 function Upload() {
+  const [selectedFile, setSelectedFile] = useState(null); // this is the file that is selected
+  const [isFileSelected, setIsFileSelected] = useState(false); // this is a boolean to check if a file is selected or not
 
-    const [selectedFile, setSelectedFile] = useState(null);         // this is the file that is selected
-    const [isFileSelected, setIsFileSelected] = useState(false);    // this is a boolean to check if a file is selected or not
+  const changeSelection = async (e) => {
+    // this function is called when a file is selected or changed in the input
+    const file = await e.target.files[0];
+    setSelectedFile(file);
+    setIsFileSelected(true);
+    console.log(file); // this is the file that is selected
+  };
 
-    const changeSelection = async(e) => {    // this function is called when a file is selected or changed in the input
-        const file = await e.target.files[0]
-        setSelectedFile(file);
-        setIsFileSelected(true);
-        console.log(file)            // this is the file that is selected
-    }
+  const uploadFile = () => {
+    // this function is called when the submit button is clicked
+    console.log("uploading file");
+    const data = new FormData();
+    data.append("file", selectedFile);
+    axios
+      .post("http://localhost:5000/upload", data, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log({'status':res.statusText, 'data':res.data});
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-    const uploadFile = () => {              // this function is called when the submit button is clicked
-        console.log('uploading file');
-    }
+  return (
+    <div id="uploadSection">
+      <h1>Upload</h1>
+      <input id="input_file" onChange={changeSelection} type="file" />
 
-    return (
-    <div id='uploadSection'>
-        <h1>Upload</h1>
-        <input id='input_file' onChange={changeSelection} type="file" />
-
-        <button id='submit' onClick={uploadFile}>Submit</button>
+      <button id="submit" onClick={uploadFile}>
+        Submit
+      </button>
     </div>
-  )
+  );
 }
 
-export default Upload 
+export default Upload;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const fileupload = require("express-fileupload");
+const cors = require("cors");
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(fileupload({ createParentPath: true }));
+app.use(cors());
+app.use(fileupload());
+app.use(express.static("files"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+ 
 
-app.post('/upload', (req, res) => {
-    console.log(req);
-    res.send('Hello World!');
+app.post('/upload1', (req, res) => {
+    console.log(req.body);
+});
+
+
+app.post("/upload", (req, res) => {
+  const newpath = __dirname + "/files/";
+  const file = req.files.file;
+  const filename = file.name;
+ 
+  file.mv(`${newpath}${filename}`, (err) => {
+    if (err) {
+      res.status(500).send({ message: "File upload failed", code: 200 });
+    }
+    res.status(200).send({ message: "File Uploaded", code: 200 });
+  });
+});
+
+app.get('/', (req,res)=>{
+    res.send('Hello World');
 })
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+ 
+app.listen(4443, () => {
+  console.log("Server running successfully on 4443");
+});
